c3c42fb442e4a84ae7229308c8a8e90a49634091
Achref <medachrefg@gmail.com>
2022-02-28 15:23:14 +0100

    new commit

--- src/components/FeedbackList.js
+++ src/components/FeedbackList.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { motion, animatePresence, AnimatePresence } from 'framer-motion';
+import { motion, AnimatePresence } from 'framer-motion';
 import { useContext } from 'react';
 import FeedbackContext from '../context/FeedbackContext';
 import FeedbackItem from './FeedbackItem';
